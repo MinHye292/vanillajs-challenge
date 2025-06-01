c4293530ec5aec0681e69ed5dd578815fc9f285d
MinHye292 <8547364@naver.com>
2025-06-02 01:08:36 +0900

    button 클릭 시 타겟 확인
    리스트 생성 시 삭제 버튼 생성, 삭제 버튼 타겟 방법

--- js/todo.js
+++ js/todo.js
@@ -2,14 +2,26 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+}
+
 function paintToDo(newTodo) {
-  console.log("111");
   const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.innerText = newTodo;
+  const button = document.createElement("button");
+  button.innerText = "X";
+  button.addEventListener("click", deleteToDo);
+  li.appendChild(span);
+  li.appendChild(button);
+  toDoList.appendChild(li);
 }
 
 function handleToDoSubmit(event) {
   event.preventDefault();
-  const newTodo = toDoList.value;
+  const newTodo = toDoInput.value;
   toDoInput.value = "";
   paintToDo(newTodo);
 }
